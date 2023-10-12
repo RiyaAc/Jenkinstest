d6546c9322202d0985bc0c9e42dafef8f20204bf
RiyaAc <96793405+RiyaAc@users.noreply.github.com>
2023-10-12 11:51:00 +0530

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -2,6 +2,8 @@ package com.example;
 
 /**
  * This is a class.
+ *
+ * This constructor is currently empty. You should either add initialization logic or provide a comment explaining why it's empty.
  */
 public class Greeter {
 
@@ -9,9 +11,14 @@ public class Greeter {
    * This is a constructor.
    */
   public Greeter() {
-
+ // You can add initialization logic here if needed
   }
-
+/**
+   * Greets the specified person.
+   *
+   * @param someone The name of the person to greet.
+   * @return A greeting message.
+   */
   //TODO: Add javadoc comment
   public String greet(String someone) {
     return String.format("Hello, %s!", someone);
